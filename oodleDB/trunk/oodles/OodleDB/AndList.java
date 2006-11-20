40b421a347201730ee117b56a560fb38107ff874
williams <williams@5aa1a5ca-6722-0410-b23d-a531b5d4bfc0>
2006-11-20 22:38:04 +0000

    Added AndList and OrList 'concrete' implementations

---
+++ oodleDB/trunk/oodles/OodleDB/AndList.java
@@ -0,0 +1,11 @@
+package oodles.OodleDB;
+
+
+public class AndList implements ConditionList {
+
+	public void add(ConditionList cl) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ oodleDB/trunk/oodles/OodleDB/OrList.java
@@ -0,0 +1,11 @@
+package oodles.OodleDB;
+
+
+public class OrList implements ConditionList {
+
+	public void add(ConditionList cl) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

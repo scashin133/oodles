fc7e7dcb07a182ee9ef4e5d7be73aa535aabef59
rico <rico@5aa1a5ca-6722-0410-b23d-a531b5d4bfc0>
2006-11-26 08:12:06 +0000

    Basic Structure of OodleDBC

---
+++ oodleDBC/trunk/oodles/DBC/OodleDataSource.java
@@ -0,0 +1,48 @@
+package oodles.DBC;
+
+/*
+ * OodleDataSource is an object which creates Connection objects.  The OodleDataSource must be
+ */
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+public class OodleDataSource implements DataSource {
+	
+	private DatabaseServer databaseServer;
+
+	public Connection getConnection() throws SQLException {
+		
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Connection getConnection(String arg0, String arg1)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrintWriter getLogWriter() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getLoginTimeout() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void setLogWriter(PrintWriter arg0) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setLoginTimeout(int arg0) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+}

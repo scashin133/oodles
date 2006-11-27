3031ed66dbaeeaa126a7b8c0a33a40dc3d119799
archer <archer@5aa1a5ca-6722-0410-b23d-a531b5d4bfc0>
2006-11-27 01:57:42 +0000

    QueryEntryWizard 2/3 steps formed.  Filter step needs to be added. Comments will come later.

--- oodleFace/trunk/QueryEntryWizardManager.java
+++ oodleFace/trunk/QueryEntryWizardManager.java
@@ -1,17 +1,33 @@
+import java.awt.Component;
+import java.awt.ItemSelectable;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTable;
 import org.pietschy.wizard.PanelWizardStep;
 import org.pietschy.wizard.Wizard;
 import org.pietschy.wizard.models.StaticModel;
 
 
-public class QueryEntryWizardManager extends JPanel implements ActionVisualization {
+public class QueryEntryWizardManager extends JPanel implements ActionVisualization, ActionListener{
 
 	Wizard myWizard;
+	String queryTable;
+	ArrayList<String> queryColumns = new ArrayList<String>();
 	String myQuery = "COMPILEDQUERY";
+	
+	// PanelWizardSteps (for access by action method
+	PanelWizardStep tableSelect = new PanelWizardStep();
+	PanelWizardStep columnSelect = new PanelWizardStep();
+	PanelWizardStep filterSelect = new PanelWizardStep();
 	public QueryEntryWizardManager(){
 		super();
 		createWizard();
@@ -21,22 +37,47 @@ public class QueryEntryWizardManager extends JPanel implements ActionVisualizati
 		StaticModel myModel = new StaticModel();
 		
 		// Select which table
-		PanelWizardStep tableSelect = new PanelWizardStep();
+		
 		tableSelect.add(new JLabel("TableSelect"));
 		tableSelect.setName("Select Table");
+		ButtonGroup tableButtons = new ButtonGroup();
+		// TODO this will eventually add a button for each table in the DB
+		for (int t=0; t<6; t++){
+			String tableName = "Table" + t;
+			JRadioButton newButton = new JRadioButton(tableName);
+		    newButton.setActionCommand(tableName);
+		    newButton.setName("TableButton" + tableName);
+		    newButton.addActionListener(this);
+		    tableButtons.add(newButton);
+		    tableSelect.add(newButton);
+		}
 		myModel.add(tableSelect);
 		
 		// Select Columns to display
-		PanelWizardStep columnSelect = new PanelWizardStep();
+		
 		columnSelect.add(new JLabel("ColumnSelect"));
 		columnSelect.setName("Select Columns");
+		
+		for (int c=0; c<6; c++){
+			String columnName = "Column" + c;
+			JCheckBox newBox = new JCheckBox(columnName);
+			newBox.setActionCommand(columnName);
+			newBox.setName("ColumnBox" + columnName);
+			newBox.addActionListener(this);
+			columnSelect.add(newBox);
+		}
 		
 		myModel.add(columnSelect);
 		
 		// Filter values
-		PanelWizardStep filterSelect = new PanelWizardStep();
+		
 		filterSelect.add(new JLabel("ColumnSelect"));
 		filterSelect.setName("Set Filters");
+		
+		JButton skipButton = new JButton("Skip");
+		skipButton.setName("Skip");
+		skipButton.addActionListener(this);
+		filterSelect.add(skipButton);
 		
 		myModel.add(filterSelect);
 		
@@ -62,6 +103,31 @@ public class QueryEntryWizardManager extends JPanel implements ActionVisualizati
 
 	public boolean launch() {
 		return true;
+	}
+	
+	public void actionPerformed(ActionEvent a) {
+		Component source = (Component)a.getSource();
+		
+		if (source.getName().startsWith("TableButton")){
+			queryTable = a.getActionCommand();
+			tableSelect.setComplete(true);
+		}
+		
+		if (source.getName().startsWith("ColumnBox")){
+			ItemSelectable checkBoxSource = (ItemSelectable)source;
+			if (checkBoxSource.getSelectedObjects() != null){
+				queryColumns.add(a.getActionCommand());
+			}
+			else {
+				queryColumns.remove(a.getActionCommand());
+			}
+			columnSelect.setComplete(true);
+		}
+		
+		if (source.getName().startsWith("Skip")){
+			filterSelect.setComplete(true);
+		}
+
 	}
 
 }

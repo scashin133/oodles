434723ce0aa98ac76cd872efc8c829c9d493f05d
williams <williams@5aa1a5ca-6722-0410-b23d-a531b5d4bfc0>
2006-11-28 21:29:03 +0000

    Partial implementation of OodleDBC

--- oodleDBC/trunk/oodles/DBC/OodleConnection.java
+++ oodleDBC/trunk/oodles/DBC/OodleConnection.java
@@ -1,5 +1,10 @@
 package oodles.DBC;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
@@ -8,202 +13,329 @@ import java.sql.SQLException;
 import java.sql.SQLWarning;
 import java.sql.Savepoint;
 import java.sql.Statement;
+
 import java.util.Map;
 
 import oodles.RMICommon.RemoteDatabase;
+import oodles.RMICommon.RemoteDatabaseServer;
 
+/**
+ * <p>
+ * Manages a connection (and it's session) to an OodleDB database.
+ * </p>
+ * 
+ * <strong>JDBC DISCLAIMER:</strong>
+ * 
+ * <p>
+ * The packages java.sql and javax.sql are collectively known as 
+ * "JDBC"
+ * </p>
+ * 
+ * <p>
+ * JDBC's interfaces include many, many methods which provide lots of
+ * wonderfully intricate functionality which have been deemed totally
+ * unecessary for OodleDBC. These methods will throw SQLExceptions when
+ * invoked, and have been marked as "Unsupported"
+ * </p>
+ * 
+ * @author mitch
+ */
 public class OodleConnection implements Connection {
 	
-	private RemoteDatabase database;
+	protected RemoteDatabaseServer databaseServer;
+	protected RemoteDatabase database;
 
-	public void clearWarnings() throws SQLException {
-		// TODO Auto-generated method stub
+	/** 
+	 * The RMI Registry object  --
+	 * This object is a cross-RMI proxy which provides a String -> Remote map.
+	 */
+	protected Registry registryServer;
+	
+	
+	/**
+	 * Constructs a new OodleConnection
+	 * 
+	 * @param hostName The host name that this connection should connect to
+	 * @param databaseName The database name that this connection should use, or null to not specify a database
+	 * 
+	 * @throws IllegalArgumentException
+	 */
+	public OodleConnection(String hostName, String databaseName) throws IllegalArgumentException {
+		
+		/* Attempt to connect to the RMI registry on the remote */
+		
+		try {
+			registryServer = LocateRegistry.getRegistry(hostName);
+		} catch (RemoteException e) {
+			throw new IllegalArgumentException("Unable to connect to host: '" + hostName + "'");
+		}
+		
+		
+		/* Attempt to lookup the database server */
+		
+		try {
+			databaseServer = (RemoteDatabaseServer) registryServer.lookup("OodleDB");
+		} catch (NotBoundException e) {
+			throw new IllegalArgumentException("'" + hostName + "' is not an OodleDB server.");
+		} catch (Exception e) {
+			throw new IllegalArgumentException("Connected to host '" + hostName + "', but failed to initialize connection.");
+		}
 
+		
+		/* If a database was specified attempt to use it */
+		
+		if (databaseName != null) {
+			useDatabase(databaseName);
+		}
+		
+		
 	}
-
-	public void close() throws SQLException {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void commit() throws SQLException {
-		// TODO Auto-generated method stub
-
-	}
-
+	
+	
+	/**
+	 * Creates a new statement object for interacting with the database.
+	 * 
+	 * @return A new statement object.
+	 */
 	public Statement createStatement() throws SQLException {
-		// TODO Auto-generated method stub
+		// TODO: Implement : /
 		return null;
 	}
+	
 
+	/**
+	 * Returns the database server that this connection is with.
+	 * 
+	 * @return The RemoteDatabaseServer object.
+	 * 
+	 * @see oodles.RMICommon.RemoteDatabaseServer
+	 */
+	public RemoteDatabaseServer getDatabaseServer() {
+		return databaseServer;
+	}
+	
+	
+	/**
+	 * Returns the RemoteDatabase object representing the database that's currently 
+	 * in use for this session, or null if no database is currently in use.
+	 * 
+	 * @return the RemoteDatabase object for the database currently in use, or null if there is no
+	 * currently selected database.
+	 * 
+	 * @see oodles.RMICommon.RemoteDatabase
+	 */
+	public RemoteDatabase getDatabase() {
+		return database;
+	}
+	
+	
+	/**
+	 * Changes the database in use for this session.
+	 * 
+	 * @param databaseName The name of the database to use
+	 * 
+	 * @throws IllegalArgumentException If there is a problem changing the database in use.
+	 */
+	public void useDatabase(String databaseName) throws IllegalArgumentException {
+		
+		try {
+			database = (RemoteDatabase) registryServer.lookup("OodleDB." + databaseName);
+		} catch (NotBoundException e) {
+			throw new IllegalArgumentException("Couldn't find a database named '" + databaseName + "' on the server.");
+		} catch (Exception e) {
+			throw new IllegalArgumentException("There was an error looking up the database.");
+		}
+		
+	}
+	
+	
+	/*
+	 * And now to make JDBC happy...
+	 */
+	
+	/** Unsupported */
+	public void close() throws SQLException {
+		throw new SQLException("Unsupported Operation.");
+	}
+	
+	/** Unsupported */
+	public void clearWarnings() throws SQLException {
+		throw new SQLException("Unsupported Operation.");
+	}
+
+	/** Unsupported */
+	public void commit() throws SQLException {
+		throw new SQLException("Unsupported Operation.");
+	}
+
+	/** Unsupported */
 	public Statement createStatement(int arg0, int arg1) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public Statement createStatement(int arg0, int arg1, int arg2)
 			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
-
+	
+	/** Unsupported */
 	public boolean getAutoCommit() throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public String getCatalog() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public int getHoldability() throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public DatabaseMetaData getMetaData() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public int getTransactionIsolation() throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public Map<String, Class<?>> getTypeMap() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public SQLWarning getWarnings() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public boolean isClosed() throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public boolean isReadOnly() throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public String nativeSQL(String sql) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public CallableStatement prepareCall(String sql) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public CallableStatement prepareCall(String sql, int resultSetType,
 			int resultSetConcurrency) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public CallableStatement prepareCall(String sql, int resultSetType,
 			int resultSetConcurrency, int resultSetHoldability)
 			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public PreparedStatement prepareStatement(String sql) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public PreparedStatement prepareStatement(String sql, int autoGeneratedKeys)
 			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public PreparedStatement prepareStatement(String sql, int[] columnIndexes)
 			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public PreparedStatement prepareStatement(String sql, String[] columnNames)
 			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public PreparedStatement prepareStatement(String sql, int resultSetType,
 			int resultSetConcurrency) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public PreparedStatement prepareStatement(String sql, int resultSetType,
 			int resultSetConcurrency, int resultSetHoldability)
 			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public void releaseSavepoint(Savepoint savepoint) throws SQLException {
-		// TODO Auto-generated method stub
-
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public void rollback() throws SQLException {
-		// TODO Auto-generated method stub
-
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public void rollback(Savepoint savepoint) throws SQLException {
-		// TODO Auto-generated method stub
-
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public void setAutoCommit(boolean autoCommit) throws SQLException {
-		// TODO Auto-generated method stub
-
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public void setCatalog(String catalog) throws SQLException {
-		// TODO Auto-generated method stub
-
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public void setHoldability(int holdability) throws SQLException {
-		// TODO Auto-generated method stub
-
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public void setReadOnly(boolean readOnly) throws SQLException {
-		// TODO Auto-generated method stub
-
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public Savepoint setSavepoint() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public Savepoint setSavepoint(String name) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public void setTransactionIsolation(int level) throws SQLException {
-		// TODO Auto-generated method stub
-
+		throw new SQLException("Unsupported Operation.");
 	}
 
+	/** Unsupported */
 	public void setTypeMap(Map<String, Class<?>> map) throws SQLException {
-		// TODO Auto-generated method stub
-
+		throw new SQLException("Unsupported Operation.");
 	}
 
 }
--- oodleDBC/trunk/oodles/DBC/OodleStatement.java
+++ oodleDBC/trunk/oodles/DBC/OodleStatement.java
@@ -1,21 +1,383 @@
 package oodles.DBC;
 
+import java.rmi.RemoteException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLWarning;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import oodles.RMICommon.ColumnSchema;
+
+/**
+ * 
+ * 
+ * <strong>JDBC DISCLAIMER:</strong>
+ * 
+ * <p>
+ * The packages java.sql and javax.sql are collectively known as 
+ * "JDBC"
+ * </p>
+ * 
+ * <p>
+ * JDBC's interfaces include many, many methods which provide lots of
+ * wonderfully intricate functionality which have been deemed totally
+ * unecessary for OodleDBC. These methods will throw SQLExceptions when
+ * invoked, and have been marked as "Unsupported"
+ * </p>
+ * 
+ * @author mitch
+ *
+ */
 public class OodleStatement implements Statement {
+	
+
+	/** The connection object that created this OodleStatement instance */
+	protected OodleConnection parentConnection;
+	
+	/** Caches the ResultSet from the last query we executed, or is null if the last query didn't have a ResultSet */ 
+	protected ResultSet currentResultSet; 
+	
+	
+	/** The pattern for a column schema */
+	protected static final Pattern COLUMN_SCHEMA_PATTERN = 
+		Pattern.compile("(\\w+)\\s+(\\w+)");
+	
+	
+	/** The pattern for a USE query */
+	protected static final Pattern USE_QUERY_PATTERN = 
+		Pattern.compile("use\\s+(\\w+)");
+	
+	
+	/** The pattern for an ALTER TABLE query */
+	protected static final Pattern ALTER_QUERY_PATTERN = 
+		Pattern.compile("alter\\s+table\\s+(\\w+)\\s+add\\s+column\\s+(\\w+)\\s+(\\w+)",
+		Pattern.CASE_INSENSITIVE);
+	
+	
+	/** The pattern for an CREATE query */
+	protected static final Pattern CREATE_QUERY_PATTERN = 
+		Pattern.compile("create\\s+(database)|(table)\\s+(.*)",
+		Pattern.CASE_INSENSITIVE);
+	
+	
+	// TODO: SELECT query patterns
+	
+	// TODO: DELETE query patterns
+	
+	// TODO: UPDATE query patterns
+	
+	// TODO: DESCRIBE query patterns
+	
+	// TODO: INSERT query patterns
+	
+	// TODO: SHOW query patterns
+	
+	// TODO: DROP query patterns
+	
+	/** The pattern for the table definition clause of a CREATE TABLE query */
+	protected static final Pattern TABLE_DEFINITION_CLAUSE_PATTERN = 
+		Pattern.compile("(\\w+)\\s+(.*)",
+		Pattern.CASE_INSENSITIVE);
+	
+	
+	/** The pattern for the table schema clause of a CREATE TABLE query */
+	protected static final Pattern TABLE_SCHEMA_CLAUSE_PATTERN = 
+		Pattern.compile("(\\w+\\s+\\w+)(,\\s+\\w+\\s+\\w+)*",
+		Pattern.CASE_INSENSITIVE);
+	
+	
+	
+	
+
+	
+	public OodleStatement(OodleConnection connection) throws IllegalArgumentException {
+		
+		/* Make sure we have an actual connection */
+		
+		if (connection == null) {
+			throw new IllegalArgumentException("Connection cannot be null.");
+		}
+		
+		this.parentConnection = connection; 
+
+	}
+	
+	
+	/**
+	 * Returns the ResultSet from the last query, or null if the last
+	 * query didn't return a result set.
+	 * 
+	 * @returns ...
+	 */
+	public ResultSet getResultSet() throws SQLException {
+		return currentResultSet;
+	}
+	
+	
+	/**
+	 * Performs a query on the database.
+	 */
+	public boolean execute(String query) throws SQLException {
+		
+		/* Clear out the current result set. */
+		
+		currentResultSet = null;
+		
+		/* Peek at the query and hand it off to a helper */
+		
+		
+		try {
+		
+			if ( USE_QUERY_PATTERN.matcher(query).matches() ) {
+	
+				executeUseQuery(query);
+				
+			} else if ( ALTER_QUERY_PATTERN.matcher(query).matches() ) {
+				
+				executeAlterQuery(query);
+				
+			} else if ( CREATE_QUERY_PATTERN.matcher(query).matches() ) {	
+				
+				executeCreateQuery(query);
+				
+			} else {
+				
+				throw new SQLException("Query was malformed.");
+				
+			}
+		
+		} catch (RemoteException e ) {
+		
+			/* If a RemoteException is thrown, RMI had problems and couldn't invoke the remote method */
+			
+			throw new SQLException("Error communicating with database server.");
+			
+		}
+		
+		return true;
+		
+	}
+	
+	
+	
+	
+	
+	/**
+	 * Changes the database
+	 * 
+	 * @param query
+	 * @throws SQLException
+	 */
+	protected void executeUseQuery(String query) throws SQLException, RemoteException {
+		
+		Matcher m = USE_QUERY_PATTERN.matcher(query);
+		
+		/* Make sure we match */
+		
+		if (m.matches() == false) {
+			throw new SQLException("USE query was malformed.");
+		}
+		
+		/* Set the parent connection to use the new database */
+		
+		String databaseName = m.group(1);
+		
+		parentConnection.useDatabase(databaseName);
+		
+	}
+	
+	/**
+	 * Alter query
+	 * 
+	 * @param query
+	 * @throws SQLException
+	 */
+	protected void executeAlterQuery(String query) throws SQLException, RemoteException {
+		
+		Matcher m = ALTER_QUERY_PATTERN.matcher(query);
+		
+		/* Make sure we match */
+		
+		if (m.matches() == false) {
+			throw new SQLException("ALTER query was malformed.");
+		}
+		
+		/* "Alter" the table */
+		
+		String tableName = m.group(1);
+		ColumnSchema columnSchema = new ColumnSchema(m.group(2), m.group(3));
+		
+		parentConnection.getDatabase().alterTable(tableName, columnSchema);
+		
+	}
+	
+	
+	/**
+	 * <p>
+	 * 	Executes a CREATE query.
+	 * </p>
+	 * 
+	 * @param query the query.
+	 * @throws SQLException if there is a problem parsing the query.
+	 */
+	protected void executeCreateQuery(String query) throws SQLException, RemoteException {
+		
+		
+		Matcher m = CREATE_QUERY_PATTERN.matcher(query);
+		
+		/* Make sure we match */
+		
+		if (m.matches() == false) {
+			throw new SQLException("CREATE query was malformed.");
+		}
+		
+		
+		
+		if (m.group(1) != null) {
+			
+			/* If it's a CREATE DATABASE query */
+			
+			String databaseName = m.group(3);
+			
+			/*  make sure the database name was ok */
+			
+			if ( Pattern.matches("\\w+", databaseName) == false ) {
+				throw new SQLException("CREATE DATABASE query was malformed.");
+			}
+			
+			/* Create the database */
+			
+			parentConnection.getDatabaseServer().createDatabase(databaseName);
+			
+		} else {
+			
+			/* If it's a CREATE TABLE query */
+			
+			String tableDefinitionClause = m.group(3);
+			
+			
+			/* Make sure we've got a table name and a list of column schema */
+			
+			Matcher tableDefinitionMatcher = TABLE_DEFINITION_CLAUSE_PATTERN.matcher(tableDefinitionClause);
+			
+			if ( tableDefinitionMatcher.matches() == false ) {
+				throw new SQLException("CREATE TABLE query was malformed.");
+			}
+			
+			
+			/* Parse the table definition */
+			
+			String tableName = tableDefinitionMatcher.group(1);
+			String tableSchemaClause = tableDefinitionMatcher.group(2);
+
+			
+			/* A comma delimited list of column definitions: NAME TYPE, NAME TYPE ... */
+			
+			if ( TABLE_SCHEMA_CLAUSE_PATTERN.matcher( tableSchemaClause ).matches() ) {
+				
+				/* Split into column schemas */
+				
+				String[] columnDefinitions = tableSchemaClause.split(",\\s+");
+				
+				ArrayList<ColumnSchema> columnSchemaList = new ArrayList<ColumnSchema>(columnDefinitions.length);
+				
+
+				/* Iterate over the schema and add them to the list */
+				
+				for (String columnSchema : columnDefinitions) {
+					columnSchemaList.add(parseColumnSchema(columnSchema));
+				}
+				
+				
+				/* Create the table */
+				
+				parentConnection.getDatabase().createTable(tableName, columnSchemaList);
+				
+				
+			} else {
+				
+				/* The table definition was malformed */
+				
+				throw new SQLException("CREATE TABLE query table definition was malformed.");
+			}
+			
+		}
+		
+		
+	}
+	
+	/**
+	 * A helper method that constructs a ColumnSchema object from a column schema.
+	 * 
+	 * @param columnSchema the column schema string.
+	 * 
+	 * @return A new column schema object from the column schema string
+	 * 
+	 * @throws SQLException if there was an error 
+	 */
+	protected static ColumnSchema parseColumnSchema(String columnSchema) throws SQLException, RemoteException {
+		
+		Matcher columnSchemaMatcher = COLUMN_SCHEMA_PATTERN.matcher(columnSchema);
+		
+		if (columnSchemaMatcher.matches()) {
+			
+			String name = columnSchemaMatcher.group(1);
+			String dataType = columnSchemaMatcher.group(2);
+			
+			return new ColumnSchema(name, dataType);
+			
+		} else {
+			throw new SQLException("Column definition was malformed.");
+		}
+		
+		
+	}
+	
+	
+	protected void executeDeleteQuery(String query) throws SQLException, RemoteException {
+		
+	}
+	
+	protected void executeDescribeQuery(String query) throws SQLException, RemoteException {
+		
+	}
+	
+	protected void executeDropQuery(String query) throws SQLException, RemoteException {
+		
+	}
+	
+	protected void executeInsertQuery(String query) throws SQLException, RemoteException {
+		
+	}
+	
+	protected void executeSelectQuery(String query) throws SQLException, RemoteException {
+		
+	}
+	
+	protected void executeShowQuery(String query) throws SQLException, RemoteException {
+		
+	}
+	
+
+	protected void executeUpdateQuery(String query) throws SQLException, RemoteException {
+		
+	}
+	
+	
+	/*
+	 * And now to make JDBC happy...
+	 */
 
 	public void addBatch(String arg0) throws SQLException {
-		// TODO Auto-generated method stub
-
+		throw new SQLException("Unsupported Operation.");
 	}
 
 	public void cancel() throws SQLException {
-		// TODO Auto-generated method stub
-
+		throw new SQLException("Unsupported Operation.");
 	}
 
 	public void clearBatch() throws SQLException {
@@ -33,10 +395,6 @@ public class OodleStatement implements Statement {
 
 	}
 
-	public boolean execute(String arg0) throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
 	public boolean execute(String arg0, int arg1) throws SQLException {
 		// TODO Auto-generated method stub
@@ -130,11 +488,6 @@ public class OodleStatement implements Statement {
 	public int getQueryTimeout() throws SQLException {
 		// TODO Auto-generated method stub
 		return 0;
-	}
-
-	public ResultSet getResultSet() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	public int getResultSetConcurrency() throws SQLException {

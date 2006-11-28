57c15a2b3a65f0420c1bf19c38a2ecc8488c608a
williams <williams@5aa1a5ca-6722-0410-b23d-a531b5d4bfc0>
2006-11-28 20:37:47 +0000

    Removed stupid shit that I added to column schema

--- OodleDB_RMI/trunk/oodles/RMICommon/ColumnSchema.java
+++ OodleDB_RMI/trunk/oodles/RMICommon/ColumnSchema.java
@@ -1,17 +1,26 @@
 package oodles.RMICommon;
 
+import java.io.Serializable;
+
 /**
  * Stores the name and datatype of a column
  * 
  * @author mitch
  */
-public class ColumnSchema {
+public class ColumnSchema implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	protected String name;
 	protected String dataType;
-
-	
-	public ColumnSchema(String name, String dataType ) {
+		
+	/**
+	 * Construts a new ColumnSchema given a name and a data type
+	 * 
+	 * @param name The name of the column
+	 * @param dataType The data type of the column
+	 */
+	public ColumnSchema(String name, String dataType) {
 		this.name = name;
 		this.dataType = dataType;
 	}
